b13827dbb6bfd79a0f4f4a34b4e473bb86af2b1c
Timothy Fisher <tim.fisher@tim.fisher>
2022-01-11 01:10:59 +0000

    feat: add state to table

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,59 @@
 <template>
   <div>
     <div class="table-container">
-      <table style="width:100%">
+      <table style="width: 100%">
         <tr>
           <th>Firstname</th>
           <th>Lastname</th>
           <th>Action</th>
         </tr>
-        <tr v-for="(person, index) in persons" :key="person.firstName + person.lastName + index">
+        <tr
+          v-for="(person, index) in persons"
+          :key="person.firstName + person.lastName + index"
+        >
           <td>{{ person.firstName }}</td>
           <td>{{ person.lastName }}</td>
-          <td><el-button @click="remove">Remove</el-button></td>
+          <td><el-button @click="remove(person)">Remove</el-button></td>
         </tr>
       </table>
     </div>
     <div class="input-container">
-      <el-input placeholder="Firstname"></el-input>
-      <el-input placeholder="Lastname"></el-input>
+      <el-input v-model="firstName" placeholder="Firstname"></el-input>
+      <el-input v-model="lastName" placeholder="Lastname"></el-input>
       <el-button @click="add">Add</el-button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Vue, Component } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
+import { ActionTypes } from "../store/persons";
+import { PersonsState, Person } from "../types/state";
+const namespace: string = "persons";
 
-@Component({
-  name: 'index',
-})
+@Component
 export default class Index extends Vue {
-  persons = [{firstName: 'Jill', lastName: 'Smith'}, {firstName: 'Eve', lastName: 'Jackson'}];
+  @Action(ActionTypes.ADD_PERSON, { namespace }) addPerson: any;
+  @Action(ActionTypes.REMOVE_PERSON, { namespace }) removePerson: any;
+  // @ts-expect-error TS2564: Property 'persons' has no initializer and is not definitely assigned in the constructor.
+  @Getter("persons", { namespace }) persons: PersonsState["persons"];
 
-  add() {}
+  private firstName: string = "";
+  private lastName: string = "";
 
-  remove() {}
+  add() {
+    this.addPerson({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    });
+    this.firstName = "";
+    this.lastName = "";
+  }
 
+  remove(person: Person) {
+    this.removePerson(person);
+  }
 }
 </script>
 
@@ -59,7 +78,9 @@ export default class Index extends Vue {
   margin-right: 2rem;
 }
 
-table, th, td {
+table,
+th,
+td {
   border: 1px solid black;
   padding: 15px;
 }
--- store/persons.ts
+++ store/persons.ts
@@ -1,12 +1,43 @@
-import { ActionContext, ActionTree, GetterTree, MutationTree } from "vuex";
+import { ActionTree, GetterTree, MutationTree } from "vuex";
 import { Person, PersonsState, RootState } from "../types/state";
 
-export const state: () => PersonsState = () => ({
-  persons: []
+export enum MutationTypes {
+  ADD_PERSON = "ADD_PERSON",
+  REMOVE_PERSON = "REMOVE_PERSON",
+}
+
+export enum ActionTypes {
+  ADD_PERSON = "ADD_PERSON",
+  REMOVE_PERSON = "REMOVE_PERSON",
+}
+
+export const state = (): PersonsState => ({
+  persons: [],
 });
 
-export const getters: GetterTree<PersonsState, RootState> = {};
+export const getters: GetterTree<PersonsState, RootState> = {
+  persons(state: PersonsState): PersonsState["persons"] {
+    return state.persons;
+  },
+};
 
-export const mutations: MutationTree<PersonsState> = {};
+export const mutations: MutationTree<PersonsState> = {
+  [MutationTypes.ADD_PERSON](state, person: Person) {
+    state.persons.push(person as never);
+  },
+  [MutationTypes.REMOVE_PERSON](state, { firstName, lastName }: Person) {
+    const index = state.persons.findIndex(
+      (person) => person.firstName == firstName && person.lastName == lastName
+    );
+    state.persons.splice(index, 1);
+  },
+};
 
-export const actions: ActionTree<PersonsState, RootState> = {};
+export const actions: ActionTree<PersonsState, RootState> = {
+  [ActionTypes.ADD_PERSON]: ({ commit }, person: Person) => {
+    commit(MutationTypes.ADD_PERSON, person);
+  },
+  [ActionTypes.REMOVE_PERSON]: ({ commit }, person: Person) => {
+    commit(MutationTypes.REMOVE_PERSON, person);
+  },
+};
